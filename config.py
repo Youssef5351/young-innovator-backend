5955dbdebcb7356a615a676b861140684fe139cc
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-12-29 00:41:36 +0200

    Refactor Firebase config to dictionary format
    Updated the Firebase configuration to use a dictionary format and added a service account key.

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+# config.py
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA8ELuuu3Y5SvW8uYgdl09uFRx1QYOAONU",
-  authDomain: "test-7d81f.firebaseapp.com",
-  databaseURL: "https://test-7d81f-default-rtdb.firebaseio.com",
-  projectId: "test-7d81f",
-  storageBucket: "test-7d81f.firebasestorage.app",
-  messagingSenderId: "975832441358",
-  appId: "1:975832441358:web:6c1f9766fe44deec93d25f",
-  measurementId: "G-4TX0N76N8N"
-};
-
-# app.config['SECRET_KEY'] = "PARTH"
-# app.config['FIREBASE_CONFIG'] = firebase_config
+firebase_config = {
+    "apiKey": "AIzaSyA8ELuuu3Y5SvW8uYgdl09uFRx1QYOAONU",
+    "authDomain": "test-7d81f.firebaseapp.com",
+    "databaseURL": "https://test-7d81f-default-rtdb.firebaseio.com",
+    "projectId": "test-7d81f",
+    "storageBucket": "test-7d81f.firebasestorage.app",
+    "messagingSenderId": "975832441358",
+    "appId": "1:975832441358:web:6c1f9766fe44deec93d25f",
+    # This must point to the Service Account JSON for project: test-7d81f
+    "serviceAccount": "serviceAccountKey.json.json" 
+}
