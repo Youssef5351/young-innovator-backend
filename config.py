0490c7aff688739f6b8d4752af210db64b3db835
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-12-29 00:38:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-firebase_config = {
-    "apiKey": "AIzaSyDXQ1yEBif-6R0ooFmgy-xMQthSyaLdY1k",
-    "authDomain": "eldcare-c4ce6.firebaseapp.com",
-    "storageBucket": "eldcare-c4ce6.appspot.com",
-    "databaseURL": "https://test-7d81f-default-rtdb.firebaseio.com/",
-    "serviceAccount": "./serviceAccountKey.json.json"
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyA8ELuuu3Y5SvW8uYgdl09uFRx1QYOAONU",
+  authDomain: "test-7d81f.firebaseapp.com",
+  databaseURL: "https://test-7d81f-default-rtdb.firebaseio.com",
+  projectId: "test-7d81f",
+  storageBucket: "test-7d81f.firebasestorage.app",
+  messagingSenderId: "975832441358",
+  appId: "1:975832441358:web:6c1f9766fe44deec93d25f",
+  measurementId: "G-4TX0N76N8N"
+};
 
 # app.config['SECRET_KEY'] = "PARTH"
 # app.config['FIREBASE_CONFIG'] = firebase_config
