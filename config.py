29cb36167e72fd3a59045bcf3b4e1b6fb1b153ff
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-12-29 02:27:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # config.py
+import os
+import json
 
 firebase_config = {
-    "apiKey": "AIzaSyA8ELuuu3Y5SvW8uYgdl09uFRx1QYOAONU",
-    "authDomain": "test-7d81f.firebaseapp.com",
-    "databaseURL": "https://test-7d81f-default-rtdb.firebaseio.com",
-    "projectId": "test-7d81f",
-    "storageBucket": "test-7d81f.firebasestorage.app",
-    "messagingSenderId": "975832441358",
-    "appId": "1:975832441358:web:6c1f9766fe44deec93d25f",
-    # This must point to the Service Account JSON for project: test-7d81f
-    "serviceAccount": "serviceAccountKey.json" 
+        "apiKey": "AIzaSyA8ELuuu3Y5SvW8uYgdl09uFRx1QYOAONU",
+        "authDomain": "test-7d81f.firebaseapp.com",
+        "databaseURL": "https://test-7d81f-default-rtdb.firebaseio.com",
+        "projectId": "test-7d81f",
+        "storageBucket": "test-7d81f.firebasestorage.app",
+        "messagingSenderId": "975832441358",
+        "appId": "1:975832441358:web:6c1f9766fe44deec93d25f",
+        "serviceAccount": json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT', '{}'))
 }
